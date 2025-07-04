702d9b999026638c5d2947975ede901aaaf8282a
juniardys <juniardysetio@gmail.com>
2025-07-04 10:52:00 +0700

    ref: define seeder sequentially

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,11 @@ import * as fs from 'fs';
 
 const file = process.env.npm_config_file;
 
+// Define seeder file here sequentially
+const seeders: string[] = [
+  'first_data.ts',
+];
+
 async function runSingleFile() {
   const seed = await import(`./seeds/${file}`);
   console.log(`[seeder] started run seed from ${file}`);
@@ -16,24 +21,22 @@ async function runSingleFile() {
 }
 
 async function runAll() {
-  const directoryPath = path.join(__dirname, 'seeds');
-  fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    files.forEach(async function (file) {
-      if (!file.endsWith('.ts')) return;
-      const seed = await import(`./seeds/${file}`);
-      console.log(`[seeder] started run seed from ${file}`);
+  for (const fileName of seeders) {
+    const seedPath = path.join(__dirname, 'seeds', fileName);
+    if (fs.existsSync(seedPath)) {
+      const seed = await import(`./seeds/${fileName}`);
+      console.log(`[seeder] started run seed from ${fileName}`);
       try {
         await seed.run();
-        console.log(`[seeder] successfully run seed from ${file}`);
+        console.log(`[seeder] successfully run seed from ${fileName}`);
       } catch (error) {
-        console.log(`[seeder] error when running seed from ${file}`);
+        console.log(`[seeder] error when running seed from ${fileName}`);
         throw error;
       }
-    });
-  });
+    } else {
+      console.log(`[seeder] seed file not found: ${fileName}`);
+    }
+  }
 }
 
 if (file) {
